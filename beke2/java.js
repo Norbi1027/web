c17038f4b88302bdfaa48ecb60e564e83c12178a
Norbi1027 <sandornorbi7@gmail.com>
2023-04-12 17:03:26 +0200

    assa

--- beke2/java.js
+++ beke2/java.js
@@ -6,7 +6,7 @@ for (var i = 0; i <sor; i++)
     tablazat += '<tr>'
     for (var j = 0; j <oszlop; j++)
     {
-        tablazat += '<td>.</td>'; 
+        tablazat += `<td class = 'cella'>${i}:${j}</td>`; 
     }
     tablazat += '</tr>';
 }
